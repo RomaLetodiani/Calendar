66dcc4381b5a4be9e989b69f48dacc018e3c856d
RomaLetodiani <roman.letodiani03@gmail.com>
2023-12-02 19:39:33 +0400

    finish consts

---
+++ src/components/shared/Consts.ts
@@ -0,0 +1,19 @@
+const days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+const leapDays = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+const WeekDays = ['MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT', 'SUN'];
+const months = [
+  'JAN',
+  'FEB',
+  'MAR',
+  'APR',
+  'MAY',
+  'JUN',
+  'JUL',
+  'AUG',
+  'SEP',
+  'OCT',
+  'NOV',
+  'DEC',
+];
+
+export { days, leapDays, WeekDays, months };
